54f501c8f18ba302b10207b38f04ec5fc6694be9
Cuesta0113 <78290201+sushi3085@users.noreply.github.com>
2022-02-24 00:39:26 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var linebot = require('linebot')
+var express = require('express')
+
+var bot = linebot({
+	"channelId" : "1656918423",
+	"channelSecret" : "bf5dd4e6a1bfe57aa6a8973ec0c72a56",
+	"channelAccessToken" : "XNUj1qPi/SwRUhXP0HqAlLcP1J3efxOF6SK5eTBhDwxP4oHWYAVSWKOfuE1KZUxO51bRlcy442SS+DwvF9wpA86ug+suW+MLCgeW/VWoKe7Ts9N3T4YjFUfvY+M2pliKPH5HYNhNq/N8yHBIJYYfZAdB04t89/1O/w1cDnyilFU="
+});
+
+
+const app = express();
+const linebotParser = bot.parser();
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
+
+bot.on('message', function(event) {
+  console.log(event); //把收到訊息的 event 印出來看看
+});
+app.post('/', linebotParser);
