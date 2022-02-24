870e3852884c8568cb9b2e542eb9c50f5299ccf5
sushi3085 <s0943020@gm.ncue.edu.tw>
2022-02-24 11:41:28 +0800

    add 圖片看不懂

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var express = require('express')
 var getJSON = require('get-json')
 
 var bot = linebot({
-	"channelId" : "1656918423",
-	"channelSecret" : "bf5dd4e6a1bfe57aa6a8973ec0c72a56",
-	"channelAccessToken" : "XNUj1qPi/SwRUhXP0HqAlLcP1J3efxOF6SK5eTBhDwxP4oHWYAVSWKOfuE1KZUxO51bRlcy442SS+DwvF9wpA86ug+suW+MLCgeW/VWoKe7Ts9N3T4YjFUfvY+M2pliKPH5HYNhNq/N8yHBIJYYfZAdB04t89/1O/w1cDnyilFU="
+	"channelId": "1656918423",
+	"channelSecret": "bf5dd4e6a1bfe57aa6a8973ec0c72a56",
+	"channelAccessToken": "XNUj1qPi/SwRUhXP0HqAlLcP1J3efxOF6SK5eTBhDwxP4oHWYAVSWKOfuE1KZUxO51bRlcy442SS+DwvF9wpA86ug+suW+MLCgeW/VWoKe7Ts9N3T4YjFUfvY+M2pliKPH5HYNhNq/N8yHBIJYYfZAdB04t89/1O/w1cDnyilFU="
 });
 
 
@@ -15,18 +15,20 @@ const linebotParser = bot.parser();
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 app.listen(process.env.PORT || 8080);
 
-bot.on('message', function(event) {
+bot.on('message', function (event) {
 	// console.log(event); //把收到訊息的 event 印出來看看
-	if(event.message.type == 'text'){
+	if (event.message.type == 'text') {
 		let msg = event.message.text;
 
 		event.reply(msg).
-		then(function(data){
-			console.log(msg);
-		}).catch(function(err){
-			console.log("ERROR_的拉");
-			event.reply("我不懂！");
-		});
+			then(function (data) {
+				console.log(msg);
+			}).catch(function (err) {
+				console.log("ERROR_的拉");
+			});
+	}
+	else {
+		event.reply("窩看不懂");
 	}
 });
 app.post('/', linebotParser);
