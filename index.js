40e7147578c75356ce85a271d7c6444f0a811f30
sushi3085 <s0943020@gm.ncue.edu.tw>
2022-02-24 10:46:15 +0800

    test change port without function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var linebot = require('linebot')
 var express = require('express')
+var getJSON = require('get-json')
 
 var bot = linebot({
 	"channelId" : "1656918423",
@@ -12,10 +13,9 @@ const app = express();
 const linebotParser = bot.parser();
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
-var server = app.listen(process.env.PORT || 8080, function() {
-	var port = server.address().port;
-	console.log("App now running on port", port);
-});
+var server = app.listen(process.env.PORT || 8080);
+
+console.log(server.address().port);
 
 bot.on('message', function(event) {
 	// console.log(event); //把收到訊息的 event 印出來看看
