b4932aca231b363a986f25621e44570a41e5ddfa
sushi3085 <s0943020@gm.ncue.edu.tw>
2022-03-15 00:03:56 +0800

    add-database

--- index.js
+++ index.js
@@ -18,9 +18,25 @@ client.connect();
 client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
 	if (err) throw err;
 	for (let row of res.rows) {
-		console.log(JSON.stringify(row));
+		// console.log(JSON.stringify(row));
 	}
 	client.end();
+});
+
+client.query('CREATE TABLE WFWFWFWFWFWFWFWFWFWFWF ( FAQ int );', (err, res)=>{
+	if(err) throw err;
+	console.log(res);
+	client.end();
+})
+
+////////////////////////
+
+const { Pool } = require('pg');
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false
+	}
 });
 
 
